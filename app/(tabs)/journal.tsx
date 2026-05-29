edc1fa8eb52c5db4a3855ce3ada9d7c5da798899
pidpoddev <cpetersonluv@gmail.com>
2026-05-29 09:54:04 -0700

    Move Journal hero card further down

--- app/(tabs)/journal.tsx
+++ app/(tabs)/journal.tsx
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
   },
   content: {
     paddingHorizontal: 20,
-    paddingTop: Platform.OS === 'web' ? 20 : 58,
+    paddingTop: Platform.OS === 'web' ? 22 : 72,
     paddingBottom: Platform.OS === 'web' ? 48 : 120,
   },
   heroCard: {
